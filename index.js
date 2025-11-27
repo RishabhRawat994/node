61ee02be9af6757de4eb86c0c8e872a6a8c02c9b
Rishabh Rawat <rawatrishabh994@gmail.com>
2025-11-28 01:48:41 +0530

    Migrate from React to Express server setup
    Replaced React app setup with Express server configuration.

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require("express");
+const app = express();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app.get("/", (req, res) => {
+  res.send("Hello from Render Deployment!");
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// REQUIRED for Render
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
